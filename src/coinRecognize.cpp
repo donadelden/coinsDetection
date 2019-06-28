1ba3f823daf61ccccca09c7eb1b623ba83fad58e
Denis Donadel <denisdonadel@gmail.com>
2019-06-28 11:55:08 +0200

    Coin recognise now it's working

--- src/coinRecognize.cpp
+++ src/coinRecognize.cpp
@@ -2,25 +2,69 @@
 // Created by denis on 10/06/19.
 //
 
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/calib3d.hpp>
 #include <iostream>
-#include <opencv2/imgproc.hpp>
+#include <list>
 
 #include "coinRecognize.h"
 
 using namespace cv;
+using namespace std;
 
 Mat coinRecognize::test(Mat img) {
 
-        // need to use a trackbar for now: https://docs.opencv.org/2.4/doc/tutorials/imgproc/imgtrans/canny_detector/canny_detector.html
+        /// max value of the area of an ellipse so that we can consider it a coin
+        //TODO: it might be set in relation with the maxArea
+        int minArea = 19000;
 
-        Mat edges;
+        Mat img_gray, img_blur, thresh, kernel, closing, cont_img;
 
-        double tl = 5; // low threshold
-        double th = 30; // high threshold
-        int apertureSize = 3; // default in 3
+        vector<vector<Point> > contours;
+        vector<Vec4i> hierarchy;
 
-        Canny(img, edges, tl, th, apertureSize);
 
-        return edges;
+        /// Convert the image in a grayscale
+        cvtColor(img, img_gray, CV_BGR2GRAY);
+
+        /// Apply a gaussian blur in order to remove noise
+        GaussianBlur(img_gray, img_blur, Size(15,15), 3);
+
+        /// Create an adaptive threshold
+        adaptiveThreshold(img_blur, thresh, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY_INV, 11, 1);
+
+        /// Create a rect kernel and use it in a morphological closure
+        kernel = getStructuringElement( MORPH_RECT, Size(3,3));
+        morphologyEx(thresh, closing, MORPH_CLOSE, kernel);
+
+        /// find all the contours of the image
+        cont_img = closing.clone();
+        findContours(cont_img, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+
+        /// for each contour print out only the ones that are coin (almost, with high probability)
+        for(auto & contour : contours){
+            double area = contourArea(contour);
+            if (area < minArea)
+                continue;
+
+            if (contour.size() < 5)
+                continue;
+
+            RotatedRect el = fitEllipse(contour);
+            ellipse(img, el, (0,255,255), 2);
+        }
+
+        imshow("Morphological Closing", closing);
+        imshow("Adaptive Thresholding", thresh);
+        namedWindow("Contours", CV_WINDOW_NORMAL);
+        imshow("Contours", img);
+
+    return img;
+
+
+
 
 }
+
--- src/main.cpp
+++ src/main.cpp
@@ -25,20 +25,88 @@
 using namespace cv;
 using namespace std;
 
+Mat src, src_gray;
+Mat dst, detected_edges;
+
+int edgeThresh = 1;
+int lowThreshold = 1;
+int const max_lowThreshold = 300;
+int ratio = 2;
+int kernel_size = 3;
+char* window_name = "Edge Map";
+
+void CannyThreshold(int, void*)
+{
+    /// Reduce noise with a kernel 3x3
+    blur( src_gray, detected_edges, Size(3,3) );
+
+    /// Canny detector
+    Canny( detected_edges, detected_edges, lowThreshold, lowThreshold*ratio, kernel_size );
+
+    /// Using Canny's output as a mask, we display our result
+    dst = Scalar::all(0);
+
+    src_gray.copyTo( dst, detected_edges);
+    imshow( window_name, dst );
+}
+
+
+void HougeThreshold(int, void*){
+    vector<Vec3f> coin;
+    HoughCircles(src_gray,coin,CV_HOUGH_GRADIENT,src_gray.rows/8,20,lowThreshold*ratio,lowThreshold,0,0 );
+
+    int l = coin.size();
+    // Get the number of coins.
+
+    cout<<"\n The number of coins is: "<<l<<"\n\n";
+
+    // To draw the detected circles.
+    for( size_t i = 0; i < coin.size(); i++ )
+    {
+        Point center(cvRound(coin[i][0]),cvRound(coin[i][1]));
+        // Detect center
+        // cvRound: Rounds floating point number to nearest integer.
+        int radius=cvRound(coin[i][2]);
+        // To get the radius from the second argument of vector coin.
+        circle(src_gray,center,3,Scalar(0,255,0),-1,8,0);
+        // circle center
+        //  To get the circle outline.
+        circle(src_gray,center,radius,Scalar(0,0,255),3,8,0);
+        // circle outline
+        cout<< " Center location for circle "<<i+1<<" :"<<center<<"\n Diameter : "<<2*radius<<"\n";
+    }
+    cout<<"\n";
+
+    src_gray.copyTo( dst, detected_edges);
+    imshow( window_name, dst );
+
+}
+
+
 int main(int argc, char *argv[])
 {
-    Mat img = imread("../images_T2/1.jpg" , CV_LOAD_IMAGE_COLOR);
+    src = imread("../images_T2/1.jpg" , CV_LOAD_IMAGE_COLOR);
 
-    if(!img.data){
+    if(!src.data){
         cout << "Error on loading the image." << endl;
         return -1;
     }
 
     coinRecognize cr;
-    Mat edges = cr.test(img);
+    Mat edges = cr.test(src);
 
-    namedWindow("Undistorted image", CV_WINDOW_NORMAL);
-    imshow("Undistorted image", edges);
+    //namedWindow("Undistorted image", CV_WINDOW_NORMAL);
+    //imshow("Undistorted image", edges);
+
+    // Create a window
+    //namedWindow( window_name, CV_WINDOW_NORMAL );
+
+    // Create a Trackbar for user to enter threshold
+    //createTrackbar( "Min Threshold:", window_name, &lowThreshold, max_lowThreshold, HougeThreshold );
+
+    // Show the image
+    //CannyThreshold(0, 0);
+    //HougeThreshold(0,0);
 
     waitKey(0);
     return 0;
